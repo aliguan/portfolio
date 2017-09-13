ec0956b9b0e42c67fdbda325c444ae1c5cfe2b44
Alice Guan <aliguan726@gmail.com>
2017-09-12 21:52:40 -0400

    Brainstorm

--- js/index.js
+++ js/index.js
@@ -1,12 +1,4 @@
-$(document).ready(() => {
 
-    $('#typing').typeIt({
-        strings: ['Hello!','I am Alice','A Full Stack Web Developer'],
-        speed: 50,
-        autoStart: false,
-    });
-
-});
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "325px";
