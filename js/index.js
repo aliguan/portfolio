da249d19b7fd08115699b5d311eae3e5855df50e
Alice Guan <aliguan726@gmail.com>
2017-09-13 23:14:30 -0400

    Add Scroll Magic

--- js/index.js
+++ js/index.js
@@ -8,3 +8,47 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+
+var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 30;
+
+function moveBackground() {
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
+
+  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+  $('.bg').css({
+    '-webit-transform': translate,
+    '-moz-transform': translate,
+    'transform': translate
+  });
+
+  window.requestAnimationFrame(moveBackground);
+}
+
+$(window).on('mousemove click', function(e) {
+
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (10 * lMouseY) / 100;
+
+});
+
+moveBackground();
+
+$(document).ready( function() {
+    //SCROLL MAGIC SCENES
+    var controller = new ScrollMagic.Controller();
+
+    var scene1 = new ScrollMagic.Scene({
+        triggerElement: '#works'
+    }).setClassToggle('#gowander', 'fadeIn')
+    .addTo(controller);
+
+});
