16079491fa6d932c8c9fca7b4299d4de211541f6
Alice Guan <aliguan726@gmail.com>
2017-09-14 16:30:54 -0400

    Add portofolio filter

--- js/index.js
+++ js/index.js
@@ -7,44 +7,55 @@ var lFollowX = 0,
     friction = 1 / 30;
 
 function moveBackground() {
-  x += (lFollowX - x) * friction;
-  y += (lFollowY - y) * friction;
+    x += (lFollowX - x) * friction;
+    y += (lFollowY - y) * friction;
 
-  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+    translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
 
-  $('.bg').css({
-    '-webit-transform': translate,
-    '-moz-transform': translate,
-    'transform': translate
-  });
+    $('.bg').css({
+        '-webit-transform': translate,
+        '-moz-transform': translate,
+        'transform': translate
+    });
 
-  window.requestAnimationFrame(moveBackground);
+    window.requestAnimationFrame(moveBackground);
 }
 
 $(window).on('mousemove click', function(e) {
-
-  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
-  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
-  lFollowY = (10 * lMouseY) / 100;
-
+    var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+    var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+    lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+    lFollowY = (10 * lMouseY) / 100;
 });
 
 moveBackground();
 
-
-    //SCROLL MAGIC SCENES
-    var controller = new ScrollMagic.Controller();
-
-    var scene1 = new ScrollMagic.Scene({
-        triggerElement: 1000,
-    }).setClassToggle('#gowander', 'fadeIn')
-    .addTo(controller);
-
-
+// Show Projects Div
 $('.projects').on('click', (e) => {
-    $('#works').slideToggle();
+    $('#works').stop().animate({
+           top: 0,
+           bottom: 0,
+       }, 1600, 'easeOutQuint'
+   );
 });
+
+// Filter Portfolio
+
+$(function() {
+	var selectedClass = "";
+
+	$(".fil-cat").click( () => {
+		selectedClass = $(this).attr("data-rel");
+        $("#portfolio").fadeTo(100, 0.1);
+		$("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+        setTimeout( () => {
+            $("."+selectedClass).fadeIn().addClass('scale-anm');
+            $("#portfolio").fadeTo(300, 1);
+    }, 300);
+
+	});
+});
+
 
 });
 
